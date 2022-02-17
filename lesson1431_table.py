3aeef1cc538cf747fd270d26daa5e9a0182d9eff
Alexey Popkov <alvlpv969@gmail.com>
2022-02-17 17:15:27 +1000

    first commit

---
+++ lesson1431_table.py
@@ -0,0 +1,2 @@
+for i in range(1, 9 + 1):
+     print(*range(i, i * 9 + 1, i), sep='\t')
---
+++ lesson1432_calculator.py
@@ -0,0 +1,37 @@
+﻿# Калькулятор - версия 1
+def main_menu():
+   choice = input('\nВычисления - цифра 1.\nВыход - любая другая клавиша: ')
+   if choice == '1':
+       pass
+   else:
+       exit()
+   operation = input('\nВыберите операцию: ')
+   if (operation != '+') and (operation != '-') and (operation != '*') and (operation != '/'):
+       print('\nОшибка: такой операции не существует. Попробуйте ещё раз.')
+       main_menu()
+   a = float(input('\nВведите первое число: '))
+   b = float(input('Введите иторое число: '))
+   calculator(operation, a, b)
+
+def calculator(operation, a, b):
+   #c = 0
+
+   if operation == '+':
+       c = a + b
+       print(f'\n{a} + {b} =', c)
+       main_menu()
+   elif operation == '-':
+       c = a - b
+       print(f'\n{a} - {b} =', c)
+       main_menu()
+   elif operation == '/':
+       c = a / b
+       print(f'\n{a} / {b} =', c)
+       main_menu()
+   elif operation == '*':
+       c = a * b
+       print(f'\n{a} * {b} =', c)
+       main_menu()
+       
+
+main_menu()
